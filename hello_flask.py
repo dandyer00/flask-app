a3519f29e66028751c54fc8e839e41c1bd6983ad
Dan Dyer <dan.dyer00@gmail.com>
2021-08-13 17:55:54 -0600

    another line

--- hello_flask.py
+++ hello_flask.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def hello_world():
     s = "<p>Hello, World! Flask is here</p>"
     s += " <p>and some more stuff</p>"
+    return s
 
+    
 if __name__ == "__main__":
     app.run()
